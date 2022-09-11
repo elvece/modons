508437f39cf0396bc01b2c79237e7e110511173d
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-09-10 18:33:31 -0600

    enhance organization and setup for styling

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,18 @@ const format = require('date-fns/format')
 
 module.exports = function (config) {
     config.addPassthroughCopy({ './src/_includes/style.css': 'style.css' })
-    // add `date` filter
+
+    config.addWatchTarget('./tailwind.config.js')
+    config.addWatchTarget('./static/css/tailwind.css')
+
+    // Merge data instead of overriding
+    config.setDataDeepMerge(true);
+
+    // add date filter for human readability
     config.addFilter('date', function (date, dateFormat) {
         return format(date, dateFormat)
     })
+
     return {
         markdownTemplateEngine: 'njk',
         dir: {
