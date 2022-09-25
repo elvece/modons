a0ce7c09188fa40a397a205180bc024bb2968b1e
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-09-24 21:19:30 -0600

    add footer and make header sticky

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const format = require('date-fns/format')
 
 module.exports = function (config) {
     config.addPassthroughCopy({ './src/_includes/style.css': 'style.css' })
+    config.addPassthroughCopy({ './src/static/img/': 'static/img/' })
 
     config.addWatchTarget('./tailwind.config.js')
     config.addWatchTarget('./static/css/tailwind.css')
